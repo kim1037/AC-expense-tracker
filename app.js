aa91ffa94dcf3cdbc4955347326c8563ab2f3416
kim1037 <pan913063@gmail.com>
2023-04-14 16:01:45 +0800

    feat: add bootstrap

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.engine("hbs", exphbs({ defaultlayout: "main", extname: "hbs" }));
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
