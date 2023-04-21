7a19f29503749248e14b20c1ebcae92e5aa459e5
kim1037 <pan913063@gmail.com>
2023-04-21 16:30:19 +0800

    feat: add login and register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
 const expense = require("./modules/expense");
+const users = require("./modules/users");
 
 router.use("/expense", expense);
+router.use("/users", users);
 router.use("/", home);
 
 module.exports = router;
