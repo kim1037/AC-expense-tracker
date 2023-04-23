71336b917c209a37a506a440711d43408f984213
kim1037 <pan913063@gmail.com>
2023-04-23 23:21:19 +0800

    feat:add google login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const FacebookStrategy = require("passport-facebook");
+const LocalStrategy = require("passport-local").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
@@ -62,6 +63,29 @@ module.exports = (app) => {
       }
     )
   );
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK,
+      },
+      function (accessToken, refreshToken, profile, done) {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
+          const randomPassword = Math.random().toString(36).slice(-8);
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) => User.create({ name, email, password: hash }))
+            .then((user) => done(null, user))
+            .catch((e) => done(e, false));
+        });
+      }
+    )
+  );
+
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -17,4 +17,20 @@ router.get(
   })
 );
 
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
+
+
 module.exports = router;
