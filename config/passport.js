cfcf5f80bb5d4e57347804b6cb2896591165f31e
kim1037 <pan913063@gmail.com>
2023-04-23 22:48:41 +0800

    feat:add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const FacebookStrategy = require("passport-facebook");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
@@ -35,6 +36,29 @@ module.exports = (app) => {
           .catch((e) => {
             done(e, false);
           });
+      }
+    )
+  );
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ["email", "displayName"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user);
+          const randomPassword = Math.random().toString(36).slice(-8);
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) => User.create({ name, email, password: hash }))
+            .then((user) => done(null, user))
+            .catch((e) => done(e, false));
+        });
       }
     )
   );
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const home = require("./modules/home");
 const expense = require("./modules/expense");
 const users = require("./modules/users");
+const auth = require("./modules/auth");
 const { authenticator } = require("../middleware/auth");
 
 router.use("/expense", authenticator, expense);
 router.use("/users", users);
+router.use("/auth", auth);
 router.use("/", authenticator, home);
 
 module.exports = router;
