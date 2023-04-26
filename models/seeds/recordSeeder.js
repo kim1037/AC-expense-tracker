626e811210936a1a46ae133b69c956badf00559e
kim1037 <pan913063@gmail.com>
2023-04-26 10:57:47 +0800

    refactor: modify seeders

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -3,61 +3,8 @@ const User = require("../user");
 const Record = require("../record");
 const db = require("../../config/mongoose");
 const bcrypt = require("bcryptjs");
-
-const SEED_USERS = [];
-
-for (let i = 1; i < 3; i++) {
-  SEED_USERS.push({
-    name: `user${i}`,
-    email: `user${i}@ac.com`,
-    password: "12345678",
-  });
-}
-
-const SEED_RECORDS = [
-  {
-    type: "income",
-    name: "Feb. Salary",
-    date: "2023-02-05",
-    amount: 87000,
-    category: "Income",
-  },
-  {
-    type: "outcome",
-    name: "Lunch",
-    date: "2023-02-07",
-    amount: 95,
-    category: "Food",
-  },
-  {
-    type: "outcome",
-    name: "Take Train",
-    date: "2023-02-08",
-    amount: 177,
-    category: "Transportation",
-  },
-  {
-    type: "outcome",
-    name: "3Days Trip",
-    date: "2023-02-10",
-    amount: 3999,
-    category: "Entertainment",
-  },
-  {
-    type: "outcome",
-    name: "Rent",
-    date: "2023-02-15",
-    amount: 15000,
-    category: "Housing",
-  },
-  {
-    type: "outcome",
-    name: "Pet's food",
-    date: "2023-02-19",
-    amount: 1000,
-    category: "Other",
-  },
-];
+const SEED_USERS = require('./user.json');
+const SEED_RECORDS = require("./record.json");
 
 db.once("open", () => {
   Promise.all(
@@ -73,6 +20,7 @@ db.once("open", () => {
           })
         )
         .then((user) => {
+          console.log('user created.')
           const userId = user._id;
           return Promise.all(
             SEED_RECORDS.map((record) => {
@@ -80,20 +28,19 @@ db.once("open", () => {
                 .lean()
                 .then((category) => {
                   const categoryId = category._id;
-                  const newRecord = Object.assign({}, record, {
+                  const finalRecord = Object.assign({}, record, {
                     userId,
                     categoryId,
                   });
-                  return Record.create(newRecord);
+                  return Record.create(finalRecord);
                 })
-                .catch((e) => console.log(e));
             })
           );
         });
     })
   )
     .then(() => {
-      console.log("done.");
+      console.log("All users and records are created.");
       process.exit();
     })
     .catch((e) => console.log(e));
